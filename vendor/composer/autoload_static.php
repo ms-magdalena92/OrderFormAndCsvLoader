84c97791a989f829cde226f9a0f3fde10ce15ab9
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-08-04 20:09:42 +0200

    autoloading classes with composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -21,6 +21,14 @@ class ComposerStaticInit70da77c42c70329094df2d0d071dbdf5
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
         ),
+        'C' => 
+        array (
+            'Core\\' => 5,
+        ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -35,6 +43,14 @@ class ComposerStaticInit70da77c42c70329094df2d0d071dbdf5
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'Core\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/Core',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/App',
         ),
     );
 
