b5570d265530522040c4cfc7ee89a1a3da434ba1
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-08-04 11:14:57 +0200

    added router dispatching method

--- Core/Router.php
+++ Core/Router.php
@@ -45,4 +45,54 @@ class Router {
         
         return false;
     }
+
+    public function dispatchRoute($url)
+    {
+        if ($this -> matchUrlToRoutes($url)) {
+            
+            $this -> createControlerAndRunActionMethod();
+            
+        } else {
+            
+            throw new \Exception('No route matched.');
+        }
+    }
+    
+    protected function createControlerAndRunActionMethod()
+    {
+        $calledController = $this -> routeParams['controller'];
+        $calledController = $this -> convertToStudlyCaps($calledController);
+        $calledController = "App\Controllers\\$calledController";
+        
+        if (class_exists($calledController)) {
+            
+            $controller = new $calledController($this -> routeParams);
+            
+            $action = $this -> routeParams['action'];
+            $action = $this -> convertToCamelCase($action);
+            
+            if (preg_match('/action$/i', $action) == 0) {
+                
+                $controller -> $action();
+                
+            } else {
+                
+                throw new \Exception("Method not found");
+            }
+            
+        } else {
+            
+            throw new \Exception("Controller not found");
+        }
+    }
+
+    protected function convertToStudlyCaps($controller)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $controller)));
+    }
+
+    protected function convertToCamelCase($action)
+    {
+        return lcfirst($this -> convertToStudlyCaps($action));
+    }
 }
