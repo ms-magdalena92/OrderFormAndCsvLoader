09510cd2569fbd1381c82144df267e212ae4fee2
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-08-05 11:09:28 +0200

    count total product price and total order amount

--- public/js/order.js
+++ public/js/order.js
@@ -22,6 +22,44 @@ $(document).ready(function() {
                 $(this).prop('checked', false);
             }
         });
+
+        calculateTotalAmount();
     });
 
+    $('#orderForm').on('input', function() {
+
+        $('tr').each(function() {
+
+            var quantity = $(this).find('input[name="quantity[]"]').val();
+            var unitPrice = $(this).find('input[name="unitPrice[]"]').val();
+    
+            if(quantity !== '' && unitPrice !== '') {
+
+                var totalPrice = quantity * unitPrice;
+
+                $(this).find('input[name="totalPrice[]"]').val(totalPrice.toFixed(2));
+
+            } else {
+
+                $(this).find('input[name="totalPrice[]"]').val('');
+            }
+        });
+
+        calculateTotalAmount();
+    });
 });
+
+function calculateTotalAmount()
+{
+    var totalAmount = 0;
+
+    $('#orderForm').find('input[name="totalPrice[]"]').each(function(i,n){
+
+        if($(n).val() !== '') {
+
+            totalAmount += parseFloat($(n).val());
+        }
+    });
+
+    $('input[name="totalAmount"]').val(totalAmount.toFixed(2));
+}
