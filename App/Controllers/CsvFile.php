c6abecbf3ec248c52df84cfbd284c0ed3e2cd9e9
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2020-08-06 18:03:50 +0200

    file input server side validation and csv conversion into an array

--- App/Controllers/CsvFile.php
+++ App/Controllers/CsvFile.php
@@ -3,12 +3,36 @@
 namespace App\Controllers;
 
 use Core\View;
-use \App\Models\Products;
+use \App\File;
 
 class CsvFile extends \Core\Controller
 {
     public function uploadAction()
     {
         View::renderTemplate('File/upload.html');
+    }
+
+    public function readAction()
+    {
+        $inputValidationError = File::validateFileInput($_FILES['csvFile']);
+
+        if($inputValidationError == '') {
+
+            $productsArray = File::convertCsvIntoArray($_FILES['csvFile']);
+
+            if (!empty($productsArray)) {
+
+                
+
+            } else {
+
+                View::renderTemplate('File/upload.html', ['fileError' => 'Cannot read the file. Please try again or check if the file is valid.']);
+            }
+
+        } else {
+
+            View::renderTemplate('File/upload.html', ['inputError' => $inputValidationError]);
+        }
+
     }
 }
